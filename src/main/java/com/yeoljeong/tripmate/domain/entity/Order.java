8d80c2eff673434f706e8253ef0e5232c61662f5
bomzae <tmzl235@naver.com>
2026-04-27 00:00:24 +0900

    feat: Order를 생성하는 정적 팩토리 메서드 추가

--- src/main/java/com/yeoljeong/tripmate/domain/entity/Order.java
+++ src/main/java/com/yeoljeong/tripmate/domain/entity/Order.java
@@ -3,9 +3,11 @@ package com.yeoljeong.tripmate.domain.entity;
 import com.yeoljeong.tripmate.domain.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UuidGenerator;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.UUID;
@@ -44,4 +46,39 @@ public class Order {
 
     @Column(name = "cancel_reason")
     private String cancelReason;
+
+
+    @Builder
+    public Order(UUID userId, OrderStatus orderStatus, UUID planUnitId, UUID productId, String productName, BigDecimal price, String companyName,
+                 String country, String state, String city, UUID scheduleId, int quantity, Date experienceDate, Timestamp cancelledAt, String cancelReason) {
+        this.userId = userId;
+        this.orderStatus = orderStatus;
+        this.planUnitId = planUnitId;
+        this.productInfo = ProductInfo.of(productId, productName, price, companyName, country, state, city, scheduleId);
+        this.quantity = quantity;
+        this.experienceDate = experienceDate;
+        this.cancelledAt = cancelledAt;
+        this.cancelReason = cancelReason;
+    }
+
+    public static Order create(UUID userId, UUID planUnitId, UUID productId, String productName, BigDecimal price, String companyName,
+                        String country, String state, String city, UUID scheduleId, int quantity, Date experienceDate) {
+        return Order.builder()
+                .userId(userId)
+                .orderStatus(OrderStatus.CONFIRMED)
+                .planUnitId(planUnitId)
+                .productId(productId)
+                .productName(productName)
+                .price(price)
+                .companyName(companyName)
+                .country(country)
+                .state(state)
+                .city(city)
+                .scheduleId(scheduleId)
+                .quantity(quantity)
+                .experienceDate(experienceDate)
+                .cancelledAt(null)
+                .cancelReason(null)
+                .build();
+    }
 }
