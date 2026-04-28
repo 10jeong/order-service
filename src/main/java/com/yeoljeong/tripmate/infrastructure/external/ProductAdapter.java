ed9b7775e9251f3ebb597142cbadb181bdae1631
bomzae <tmzl235@naver.com>
2026-04-28 12:50:46 +0900

    feat: FeignException 예외 처리 추가

--- src/main/java/com/yeoljeong/tripmate/infrastructure/external/ProductAdapter.java
+++ src/main/java/com/yeoljeong/tripmate/infrastructure/external/ProductAdapter.java
@@ -6,6 +6,7 @@ import com.yeoljeong.tripmate.domain.exception.OrderErrorCode;
 import com.yeoljeong.tripmate.exception.BusinessException;
 import com.yeoljeong.tripmate.infrastructure.external.dto.ProductResponse;
 import com.yeoljeong.tripmate.response.ApiResponse;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -19,15 +20,22 @@ public class ProductAdapter implements ProductClient {
 
     @Override
     public ProductInfoCommand getProduct(UUID productId, UUID scheduleId) {
-        ApiResponse<ProductResponse> feignResponse = productFeignClient.getProduct(productId, scheduleId);
+        try {
+            ApiResponse<ProductResponse> feignResponse = productFeignClient.getProduct(productId, scheduleId);
 
-        if (feignResponse == null || feignResponse.getData() == null) {
+            if (feignResponse == null || feignResponse.getData() == null) {
+                throw new BusinessException(OrderErrorCode.PRODUCT_NOT_FOUND);
+            }
+            ProductResponse productResponse = feignResponse.getData();
+
+            return new ProductInfoCommand(productResponse.productId(), productResponse.productName(), productResponse.companyName(),
+                    productResponse.country(), productResponse.state(), productResponse.city(), productResponse.price(),
+                    productResponse.productStatus(), productResponse.productScheduleId(), productResponse.date(), productResponse.stock(), productResponse.scheduleStatus());
+        } catch (FeignException.NotFound e) {
             throw new BusinessException(OrderErrorCode.PRODUCT_NOT_FOUND);
-        }
-        ProductResponse productResponse = feignResponse.getData();
 
-        return new ProductInfoCommand(productResponse.productId(), productResponse.productName(), productResponse.companyName(),
-                productResponse.country(), productResponse.state(), productResponse.city(), productResponse.price(),
-                productResponse.productStatus(), productResponse.productScheduleId(), productResponse.date(), productResponse.stock(), productResponse.scheduleStatus());
+        } catch (FeignException e) {
+            throw new BusinessException(OrderErrorCode.PRODUCT_SERVICE_ERROR);
+        }
     }
 }
