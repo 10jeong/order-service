a4eee1ec3e4c900c523cb0dc5ba046cbd7963fb1
bomzae <tmzl235@naver.com>
2026-04-28 11:37:19 +0900

    feat: 상품 주문 여부 확인 메서드 추가

--- src/main/java/com/yeoljeong/tripmate/infrastructure/persistence/jpa/OrderJpaRepository.java
+++ src/main/java/com/yeoljeong/tripmate/infrastructure/persistence/jpa/OrderJpaRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface OrderJpaRepository extends JpaRepository<Order, UUID> {
-
+    boolean existsByUserIdAndOrderItems_PlanUnitId(UUID userId, UUID planUnitId);
 }
