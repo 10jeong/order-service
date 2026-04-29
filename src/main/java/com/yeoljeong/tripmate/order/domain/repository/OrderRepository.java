4e7764761af0e6b09c47979c5a9109ab159eb2a2
bomzae <tmzl235@naver.com>
2026-04-29 20:46:10 +0900

    feat: 사용자 주문 단건 조회, 목록 조회 JPA 메서드 추가

--- src/main/java/com/yeoljeong/tripmate/order/domain/repository/OrderRepository.java
+++ src/main/java/com/yeoljeong/tripmate/order/domain/repository/OrderRepository.java
@@ -1,13 +1,22 @@
 package com.yeoljeong.tripmate.order.domain.repository;
 
 import com.yeoljeong.tripmate.order.domain.model.Order;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepository {
 
     // 단위 일정에 귀속된 상품을 사용자가 이미 주문한 이력이 있는지
     boolean existsByUserIdAndPlanUnitId(UUID userId, UUID planUnitId);
+
+    // 사용자의 주문 단건 조회
+    Optional<Order> findByIdAndUserId(UUID orderId, UUID userId);
+
+    // 사용자의 주문 목록 조회
+    Slice<Order> findAllByUserId(UUID userId, Pageable pageable);
 
     Order save(Order order);
 }
