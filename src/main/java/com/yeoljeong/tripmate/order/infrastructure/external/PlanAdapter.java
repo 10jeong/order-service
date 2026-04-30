d7f89128e454cce19a490f19c023e9b0cbbc3106
bomzae <tmzl235@naver.com>
2026-05-01 01:13:33 +0900

    feat: infrastructure 계층에서 내부 통신 interface 구현

---
+++ src/main/java/com/yeoljeong/tripmate/order/infrastructure/external/PlanAdapter.java
@@ -0,0 +1,37 @@
+package com.yeoljeong.tripmate.order.infrastructure.external;
+
+import com.yeoljeong.tripmate.exception.BusinessException;
+import com.yeoljeong.tripmate.order.application.client.PlanClient;
+import com.yeoljeong.tripmate.order.application.dto.command.ApprovalUserCommand;
+import com.yeoljeong.tripmate.order.domain.exception.OrderErrorCode;
+import com.yeoljeong.tripmate.order.infrastructure.external.dto.PlanParticipationResponse;
+import feign.FeignException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class PlanAdapter implements PlanClient {
+
+    private final PlanFeignClient planFeignClient;
+
+    @Override
+    public ApprovalUserCommand getPlanParticipation(UUID userId, UUID planUnitId) {
+        try {
+            PlanParticipationResponse planParticipationResponse = planFeignClient.getPlanParticipation(userId.toString(), "USER", planUnitId, userId);
+
+            if (planParticipationResponse == null) {
+                throw new BusinessException(OrderErrorCode.PLAN_PARTICIPATION_NOT_FOUND);
+            }
+
+            return new ApprovalUserCommand(planParticipationResponse.planUnitId(), planParticipationResponse.userId(), planParticipationResponse.status());
+        } catch (FeignException.NotFound e) {
+            throw new BusinessException(OrderErrorCode.PLAN_PARTICIPATION_NOT_FOUND);
+
+        } catch (FeignException e) {
+            throw new BusinessException(OrderErrorCode.PLAN_SERVICE_ERROR);
+        }
+    }
+}
