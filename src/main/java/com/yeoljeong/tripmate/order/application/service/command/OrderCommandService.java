b8dfbd7cc41ed213a9af5f707fd30b0bee36954f
bomzae <tmzl235@naver.com>
2026-05-09 00:19:37 +0900

    chore: 이벤트 객체 필드 추가에 따라 orderId 추가 구성

--- src/main/java/com/yeoljeong/tripmate/order/application/service/command/OrderCommandService.java
+++ src/main/java/com/yeoljeong/tripmate/order/application/service/command/OrderCommandService.java
@@ -74,6 +74,7 @@ public class OrderCommandService {
         OrderCreatedEvent event = new OrderCreatedEvent(
                 UUID.randomUUID(),
                 savedOrder.getUserId(),
+                savedOrder.getId(),
                 savedOrder.getOrderItems().get(0).getPlanUnitId(),
                 savedOrder.getOrderItems().get(0).getProductInfo().getProductId(),
                 savedOrder.getOrderItems().get(0).getProductInfo().getScheduleId(),
