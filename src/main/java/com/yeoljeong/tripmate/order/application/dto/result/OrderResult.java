92b03be034c82c778ceb9af2e8d230d480df254a
bomzae <tmzl235@naver.com>
2026-05-11 16:11:03 +0900

    chore: cancelReason null인 경우 조건부 처리

--- src/main/java/com/yeoljeong/tripmate/order/application/dto/result/OrderResult.java
+++ src/main/java/com/yeoljeong/tripmate/order/application/dto/result/OrderResult.java
@@ -28,7 +28,7 @@ public record OrderResult(
                         .map(OrderItemResult::from)
                         .toList())
                 .cancelledAt(order.getCancelledAt())
-                .cancelReason(String.valueOf(order.getCancelReason()))
+                .cancelReason(order.getCancelReason() != null ? order.getCancelReason().getDescription() : null)
                 .build();
     }
 
