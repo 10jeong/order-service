13d3146c4544b75554560e128661ded34be4737c
bomzae <tmzl235@naver.com>
2026-05-11 16:26:25 +0900

    chore: 사용되지 않는 import 문 제거

--- src/main/java/com/yeoljeong/tripmate/order/infrastructure/persistence/jpa/OrderJpaRepository.java
+++ src/main/java/com/yeoljeong/tripmate/order/infrastructure/persistence/jpa/OrderJpaRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
--- src/main/java/com/yeoljeong/tripmate/order/infrastructure/persistence/repositoryImpl/OrderRepositoryImpl.java
+++ src/main/java/com/yeoljeong/tripmate/order/infrastructure/persistence/repositoryImpl/OrderRepositoryImpl.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
